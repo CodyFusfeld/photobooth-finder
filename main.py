5acc01c046e3bcb9f31429c4fe184bdeb093edd7
CodyFusfeld <cjfusfeld@gmail.com>
2025-06-15 23:57:53 -0700

    feat: add user input prompt for location selection

---
+++ main.py
@@ -0,0 +1,7 @@
+# Photobooth Finder - A tool to search for local photobooth services
+
+print("Welcome to the Photobooth Finder")
+
+print("Please provide a location (city or ZIP code):")
+location = input()
+print(f"You have chosen  {location}")
